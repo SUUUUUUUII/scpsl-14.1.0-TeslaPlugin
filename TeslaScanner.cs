62816cb43935fc10d8782bcacd333f7e0aeb7002
IIIAuPMa <kalash1605@mail.ru>
2025-05-10 20:39:57 +0300

    TeslaScanner.cs

--- TeslaScanner.cs
+++ TeslaScanner.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using LabApi.Events.Arguments.PlayerEvents;
-using LabApi.Features.Interfaces;
-using LabApi.Features.Wrappers;
-using LabApi.Loader;
 using LabApi.Loader.Features.Plugins;
-using MapGeneration;
-
 
 namespace tesla
 {
     public class TeslaScanner : Plugin
     {
-
         public teslaproperties.TeslaProperties teslaProperties = new teslaproperties.TeslaProperties();
 
         public override string Name => "TeslaPLugin";
@@ -38,31 +31,22 @@ namespace tesla
 
             LabApi.Events.Handlers.ServerEvents.RoundStarted += OffTesla;
         }
-
         public void TeslaFRP(PlayerTriggeringTeslaEventArgs ev)
 
         {
-
-
             if (ev.Player.IsHuman)
             {
-
                 ev.IsAllowed = false;
-
             }
             if (ev.Player.IsSCP)
             {
                 ev.IsAllowed = true;
             }
-
-
         }
         public void OffTesla()
         {
 
             teslaProperties.DisableTesla();
-
         }
-
     }
 }
