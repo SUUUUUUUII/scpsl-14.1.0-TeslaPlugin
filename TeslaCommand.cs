4c779379d58cbf2ee6d6361487044b91a1d2e668
IIIAuPMa <kalash1605@mail.ru>
2025-05-10 20:37:04 +0300

    TeslaCommand.cs

--- TeslaCommand.cs
+++ TeslaCommand.cs
@@ -1,4 +1,4 @@
-﻿using CommandSystem;
+using CommandSystem;
 using LabApi.Features.Wrappers;
 using MEC;
 using System;
@@ -11,110 +11,74 @@ using ICommand = CommandSystem.ICommand;
 
 namespace TeslaCommand
 {
-
     [CommandHandler(typeof(RemoteAdminCommandHandler))]
     public class TeslaArgument : ICommand
     {
-
         public teslaproperties.TeslaProperties tesla = new TeslaProperties();
 
         public teslaproperties.TeslaProperties properties = new teslaproperties.TeslaProperties();
         public string Command => "tesla";
-
         public string[] Aliases => new[] { "tsl", "" };
-
         public string Description => "Controll with All Tesla Gates <color=yellow> {on} {off}";
-
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
             if (sender.CheckPermission(PlayerPermissions.PlayersManagement))
             {
                 foreach (TeslaGate tesla in TeslaGate.AllGates)
                 {
-                    if (Cassie.IsSpeaking)
-                    {
-
-                        response = "Wait until CASSIE Speak";
-
-                        return false;
-
-                    }
                     if (arguments.At(0) == "on" && arguments.Count > 1)
                     {
-
                         response = "Please put correct value";
 
                         return false;
                     }
                     if (arguments.At(0) == "off" && arguments.Count > 1)
                     {
-
                         response = "Please put correct value";
 
                         return false;
                     }
-
                     if (arguments.Count == 0)
                     {
                         response = "Please put correct value";
+
                         return false;
                     }
-
-
                     if (arguments.At(0) == "on" && arguments.Count > 0)
                     {
-
                         response = "Tesla Gates engaging";
 
-                        Timing.CallDelayed(8, () => properties.EnableTesla());
+                        Timing.CallDelayed(1, () => properties.EnableTesla());
 
                         if (tesla.enabled == true)
-
                         {
-
                             response = "Tesla Gate's already engage!";
 
                             return false;
-
-                        }
-                        Cassie.Message("pitch_00.97 . . . Attention . All pitch_01.03 Tesla pitch_01.00 gate1 are now Enabled", false, true, true, "Внимание все ворота тесла включены!");
-
+                        }                       
                         break;
                     }
                     if (arguments.At(0) == "off" && arguments.Count > 0)
                     {
                         response = "Tesla Gates disengaging";
 
-                        Timing.CallDelayed(8, () => properties.DisableTesla());
+                        Timing.CallDelayed(1, () => properties.DisableTesla());
 
                         if (tesla.enabled == false)
                         {
-
                             response = "Tesla Gate's already disengage!";
 
                             return false;
-
-
-                        }
-                        Cassie.Message("pitch_00.97 . . . Attention . All pitch_01.03 Tesla pitch_01.00 gate1 are now Disabled", false, true, true, "Внимание все ворота тесла отключены!");
-
+                        }                        
                         break;
                     }
-
-
                 }
-
-
             }
 
             response = null;
 
             return false;
         }
-
-
-
     }
-
-
 }
+
