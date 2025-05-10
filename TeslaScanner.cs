c65ad71bf76ed9ba08cb651f7f824e236ce88f2f
IIIAuPMa <kalash1605@mail.ru>
2025-05-11 00:18:45 +0700

    Add files via upload

---
+++ TeslaScanner.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using LabApi.Events.Arguments.PlayerEvents;
+using LabApi.Features.Interfaces;
+using LabApi.Features.Wrappers;
+using LabApi.Loader;
+using LabApi.Loader.Features.Plugins;
+using MapGeneration;
+
+
+namespace tesla
+{
+    public class TeslaScanner : Plugin
+    {
+
+        public teslaproperties.TeslaProperties teslaProperties = new teslaproperties.TeslaProperties();
+
+        public override string Name => "TeslaPLugin";
+
+        public override string Description => "Smart Tesla and Realistic";
+
+        public override string Author => "IIIAuPMa";
+
+        public override Version Version => new Version(1, 4);
+
+        public override Version RequiredApiVersion => new Version(1, 3);
+
+        public override void Disable()
+        {
+            LabApi.Events.Handlers.PlayerEvents.TriggeringTesla -= TeslaFRP;
+
+            LabApi.Events.Handlers.ServerEvents.RoundStarted -= OffTesla;
+        }
+
+        public override void Enable()
+        {
+            LabApi.Events.Handlers.PlayerEvents.TriggeringTesla += TeslaFRP;
+
+            LabApi.Events.Handlers.ServerEvents.RoundStarted += OffTesla;
+        }
+
+        public void TeslaFRP(PlayerTriggeringTeslaEventArgs ev)
+
+        {
+
+
+            if (ev.Player.IsHuman)
+            {
+
+                ev.IsAllowed = false;
+
+            }
+            if (ev.Player.IsSCP)
+            {
+                ev.IsAllowed = true;
+            }
+
+
+        }
+        public void OffTesla()
+        {
+
+            teslaProperties.DisableTesla();
+
+        }
+
+    }
+}
