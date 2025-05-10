15412f14b00cd8c56dc431f033ea57a2e47b79b3
IIIAuPMa <kalash1605@mail.ru>
2025-05-10 20:38:34 +0300

    TeslaProperties.cs

--- TeslaProperties.cs
+++ TeslaProperties.cs
@@ -1,41 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using LabApi.Features.Wrappers;
 
 namespace teslaproperties
 {
-
     public class TeslaProperties
     {
-
         public void DisableTesla()
         {
-
             foreach (TeslaGate tesla in TeslaGate.AllGates)
             {
-
                 tesla.enabled = false;
-
-
-
             }
-
         }
         public void EnableTesla()
         {
-
             foreach (TeslaGate tesla in TeslaGate.AllGates)
             {
-
-                tesla.enabled = true;
-
+            tesla.enabled = true;
             }
-
         }
-
     }
-
 }
